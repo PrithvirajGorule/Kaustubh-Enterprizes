5f2c108d245719f765f32c78eeb432a360ab9a57
VasudhaBiradar <vasudha.biradar@mitaoe.ac.in>
2024-03-31 11:00:37 +0530

    ddo

---
+++ frontend/src/Components/Jumbotron.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Jumbotron.css'; // Import CSS for styling
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron" style={{backgroundImage: 'url("background-image-url.jpg")'}}>
+      <div className="container">
+        <h1>Welcome to Our Website</h1>
+        <p>Discover amazing products and services</p>
+        <form className="search-form">
+          <input type="text" placeholder="Search..." />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
