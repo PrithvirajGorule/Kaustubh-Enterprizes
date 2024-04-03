b74b32509cbefdb93bdcf6a5c6d22214bccca810
VasudhaBiradar <vasudha.biradar@mitaoe.ac.in>
2024-04-03 19:54:33 +0530

    jumbo

--- frontend/src/Components/Jumbotron.js
+++ frontend/src/Components/Jumbotron.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './../CSS/Jumbotron.css'; // Import CSS for styling
+ // Import CSS file for styling
 
-const Jumbotron = () => {
-  return (
-    <div className="jumbotron" style={{backgroundImage: 'url("https://static.seekingalpha.com/cdn/s3/uploads/getty_images/1368100647/image_1368100647.jpg?io=getty-c-w1280")'}}>
-      <div className="overlay"></div> {/* Add overlay div */}
-      <div className="container">
-        <h1>Welcome to Our Website</h1>
-        <p>Discover amazing products and services</p>
-      </div>
-    </div>
-  );
-};
-
-export default Jumbotron;
+ const Jumbotron = () => {
+   return (
+     <div className="jumbotron-container">
+       <div className="dark-heading">
+        
+       </div>
+       <div className="jumbotron" style={{ backgroundImage: `url('https://static.seekingalpha.com/cdn/s3/uploads/getty_images/1368100647/image_1368100647.jpg?io=getty-c-w1280')` }}>
+         <div className="overlay"></div>
+         <div className="container">
+         <h2>Welcome to our website</h2>
+         </div>
+       </div>
+     </div>
+   );
+ };
+ 
+ export default Jumbotron;
+ 
